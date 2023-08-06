cb374236da5c0d653cc00e5d76363af313be2895
unknown <lakshmisriramadari1427@gmail.com>
2023-08-07 03:43:24 +0530

    serving frontend

--- server.js
+++ server.js
@@ -441,16 +441,15 @@ app.post("/delete",cors(),async(req,res)=>{
 
 //serving the frontend
 
-app.get('/',cors(), (req, res) => {
-  res.send("hellooo");
-})
-  //   res.sendFile(path.join(__dirname,"./client/build/index.html"));
-  // });
-// app.use(express.static(path.join(__dirname,"./client/build")));
+app.get('*',cors(), (req, res) => {
+ 
+   res.sendFile(path.join(__dirname,"./client/build/index.html"));
+  });
+app.use(express.static(path.join(__dirname,"./client/build")));
 
-// app.get('*',cors(), (req, res) => {
-//   res.sendFile(path.join(__dirname,"./client/build/index.html"));
-// });
+app.get('*',cors(), (req, res) => {
+  res.sendFile(path.join(__dirname,"./client/build/index.html"));
+});
 
 
 // Start the server
